c175c743925d54c291d35e580a82bbd6fafa88a0
Bitaxa-72 <vitaly.butenev@gmail.com>
2025-03-19 10:12:51 +0900

    Add files via upload

---
+++ lesson17/src/js/app.js
@@ -0,0 +1,143 @@
+(function () {
+    // бургер
+    document.addEventListener('click', burgerInit)
+    function burgerInit(e) {
+
+
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+
+        if (document.documentElement.clientWidth > 900) return
+
+        if (burgerIcon) {
+            e.preventDefault()
+        }
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+    }
+    // модалка
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.short__link')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+    // программа        
+
+    const tabControls = document.querySelector('.tab')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab__conrol--active')) return
+
+
+        const tabContentId = tabControl.getAttribute('href')
+
+        document.querySelector('.prog__tab-content--show').classList.remove('prog__tab-content--show')
+
+        document.querySelector(tabContentId).classList.add('prog__tab-content--show')
+
+        document.querySelector('.tab__conrol--active').classList.remove('tab__conrol--active')
+
+        tabControl.classList.add('tab__conrol--active')
+    }
+    // аккордион
+
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+        });
+
+    });
+
+    // sliders 
+
+    const swiper = new Swiper('.swiper', {
+
+        spaceBetween:32,
+        slidesPerView: 4,
+    
+        pagination: {
+            el: '.gallery__pagination',
+            type:'fraction'
+        },
+
+        navigation: {
+            nextEl: '.gallery__next',
+            prevEl: '.gallery__prev',
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView:1.5,
+                spaceBetween:10
+            },
+            600: {
+                slidesPerView:3,
+                spaceBetween:20 
+            },
+            
+            900: {
+                slidesPerView:4,
+                spaceBetween:30 
+            }
+
+
+
+
+
+        }
+    });
+
+})()
